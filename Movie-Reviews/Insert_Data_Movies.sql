270cb69a31ce85f19ab1990bd9fd543fd5f84afb
ksindorf000 <ksindorf000@gmail.com>
2017-01-24 17:14:10 -0500

    Added Insert Scripts [Movie] and [Person]

---
+++ Movie-Reviews/Insert_Data_Movies.sql
@@ -0,0 +1,20 @@
+﻿ 
+INSERT
+	[dbo].[Movie]
+VALUES
+	(1,'Toy Story', 
+	'Animated', 
+	'11-22-1995', 
+	'http://www.imdb.com/title/tt0114709/?ref_=fn_al_tt_1'),
+	(2,'Jaws',
+	'Thriller',
+	'07-20-1975',
+	'http://www.imdb.com/title/tt0073195/?ref_=fn_al_tt_1'),
+	(3,'The Shawshank Redemption',
+	'Drama',
+	'10-14-1994',
+	'http://www.imdb.com/title/tt0111161/?ref_=nv_sr_1'),
+	(4,'Ernest Goes To Camp',
+	'Comedy',
+	'05-22-1987',
+	'http://www.imdb.com/title/tt0092974/?ref_=nv_sr_1');
