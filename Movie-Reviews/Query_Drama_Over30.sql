4b4421934c5e602fd11bdda57309db781dfba196
ksindorf000 <ksindorf000@gmail.com>
2017-01-24 18:00:29 -0500

    Added Hard Mode Queries

---
+++ Movie-Reviews/Query_Drama_Over30.sql
@@ -0,0 +1,12 @@
+﻿--All drama movies and ratings from ppl > 30
+		
+SELECT
+	m.*, r.Rating
+FROM
+	Movie m
+INNER JOIN
+	Review r ON r.Movie = m.Id
+INNER JOIN
+	Person p ON p.Id = r.Person
+WHERE
+	m.Genre = 'Drama' AND p.Age > 30;
---
+++ Movie-Reviews/Query_PersonData_MostRecentReleaseReview.sql
@@ -0,0 +1,17 @@
+﻿/*Get * Person Data for the people who reviewed 
+the most recently released movie*/
+
+DECLARE @MovieId AS INT = 
+	(SELECT TOP 1 Id FROM Movie 
+	ORDER BY ReleaseDate DESC);
+
+SELECT
+	p.*
+FROM
+	Person p
+INNER JOIN
+	Review r ON r.Person = p.Id
+INNER JOIN
+	Movie m ON m.Id = r.Movie
+WHERE
+	m.ID = @MovieId;
